a3d9d35590c3d06094d084552aa29a65e5d7a81a
dtgagnon <gagnon.derek@gmail.com>
2024-11-04 13:57:32 -0500

    updating for git config trying to get it to work

--- modules/home/tools/ssh/default.nix
+++ modules/home/tools/ssh/default.nix
@@ -1,9 +1,7 @@
-{
-  lib,
-  config,
-  pkgs,
-  namespace,
-  ...
+{ lib
+, config
+, namespace
+, ...
 }:
 let
   inherit (lib) mkIf;
@@ -12,15 +10,22 @@ let
 in
 {
   options.${namespace}.tools.ssh = {
-    enable = mkBoolOpt "ssh";
+    enable = mkBoolOpt true "Enable ssh";
   };
 
   config = mkIf cfg.enable {
     programs.ssh = {
+      enable = true;
+      addKeysToAgent = "ask";
       extraConfig = ''
         Host *
           HostKeyAlgorithms +ssh-ed25519
       '';
+    };
+
+    home.file = {
+      ".ssh/config.d/.keep".text = "# Managed by home-manager";
+      ".ssh/sockets/.keep".text = "# Managed by home-manager";
     };
   };
 }
