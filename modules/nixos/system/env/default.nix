ad5d38f4cfcd9f77c7410b0d187b705ede2fd50a
dtgagnon <gagnon.derek@gmail.com>
2024-11-07 19:15:50 -0500

    adding xdg env variables to all systems

---
+++ modules/nixos/system/env/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, config
+, namespace
+, ...
+}:
+let
+  inherit (lib) mkOption types;
+  cfg = config.${namespace}.system.env;
+in
+{
+  options.${namespace}.system.env = with types; mkOption {
+    type = attrsOf (oneOf [ str path (listOf (either str path)) ]);
+    apply = mapAttrs (n: v: if isList v then concatMapStringsSep ":" (x: toString x) v else (toString v));
+    default = { };
+    description = "Set environment variables for systems";
+  };
+
+  config.environment = {
+    sessionVariables = {
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_BIN_HOME = "$HOME/.local/bin";
+      XDG_DESKTOP_DIR = "$HOME";
+    };
+    variables = {
+      # To make some programs "XDG" compliant:
+      LESSHISTFILE = "$XDG_CACHE_HOME/less.history";
+      WGETRC = "$XDG_CONFIG_HOME/wgetrc";
+    };
+    extraInit = with types; concatStringsSep "\n" (mapAttrsToList (n: v: ''export ${n}="${v}"'') cfg);
+  };
+}
