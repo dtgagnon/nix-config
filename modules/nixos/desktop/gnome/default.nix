fb4e9f8e72bed70e91409a5d789624797c84e90f
dtgagnon <gagnon.derek@gmail.com>
2024-11-09 15:44:33 -0500

    added gnome and hyprland modules -hyprland config probably not ready yet

---
+++ modules/nixos/desktop/gnome/default.nix
@@ -0,0 +1,48 @@
+{ lib
+, pkgs
+, config
+, namespace
+, ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.desktop.gnome;
+in
+{
+  options.${namespace}.desktop.gnome = {
+    enable = mkBoolOpt false "Enable GNOME desktop environment";
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gnome-text-editor
+    ]) ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      gnome-calculator
+      yelp # help viewer
+      gnome-maps
+      gnome-weather
+      gnome-contacts
+      simple-scan
+    ]);
+  };
+}
