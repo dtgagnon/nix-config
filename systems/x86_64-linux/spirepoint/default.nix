0e2fed56a4a65053e9bcdfdd9535820a6b3ed336
dtgagnon <gagnon.derek@gmail.com>
2024-11-09 15:14:54 -0500

    added spirepoint system config

---
+++ systems/x86_64-linux/spirepoint/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, host
+, config
+, namespace
+, ...
+}:
+let
+  inherit (lib.${namespace}) enabled;
+  makeAdmin = if "${config.${namespace}.user.name}" == "dtgagnon" || "admin" || "root" then true else false;
+in
+{
+  imports = [ ./hardware.nix ];
+
+  snowfallorg.users.${config.spirenix.user.name} = {
+    admin = makeAdmin;
+  };
+
+  networking.hostName = "spirepoint";
+
+  spirenix = {
+    suites = {
+      common = enabled;
+      networking = enabled;
+    };
+    virtualisation = {
+      podman = enabled;
+      kvm = enabled;
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
