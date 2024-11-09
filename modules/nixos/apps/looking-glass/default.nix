0010eb22d390da59edf5dfec17d399f0b653dc5f
dtgagnon <gagnon.derek@gmail.com>
2024-11-08 21:56:22 -0500

    configured looking-glass-client

--- modules/nixos/apps/looking-glass/default.nix
+++ modules/nixos/apps/looking-glass/default.nix
@@ -1,11 +1,26 @@
 { lib
+, pkgs
 , config
 , namespace
 , ...
 }:
 let
-
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.apps.looking-glass;
+  user = config.${namespace}.user;
 in
 {
-  options = "x";
+  options.${namespace}.apps.looking-glass = {
+    enable = mkBoolOpt false "Whether or not to enable the Looking Glass client.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.looking-glass-client ];
+
+    environment.etc."looking-glass-client.ini" = {
+      user = "+${toString config.users.users.${user.name}.uid}";
+      source = ./client.ini;
+    };
+  };
 }
