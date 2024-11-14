e97eddb5bdbce450909e6f4a52f85f63fc81c850
dtgagnon <gagnon.derek@gmail.com>
2024-11-13 19:36:45 -0500

    making spirepoint-dev system config. #TODO: probably need to get rid of disko module - it'll be something that is only ran at installation time anyway

---
+++ systems/x86_64-linux/spirepoint-dev/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, config
+, namespace
+, ...
+}:
+let
+  inherit (lib.${namespace}) enabled;
+  makeAdmin = if "${config.${namespace}.user.name}" == "dtgagnon" || "admin" || "root" then true else false;
+in
+{
+  imports = [ ./hardware.nix ];
+
+  snowfallorg.users.${config.spirenix.user.name} = {
+    admin = makeAdmin;
+  };
+
+  networking.hostName = "spirepoint-dev";
+
+  spirenix = {
+    hardware = {
+      networking = enabled;
+    };
+    suites = {
+      common = enabled;
+      networking = enabled;
+    };
+    virtualisation = {
+      podman = enabled;
+      kvm = enabled;
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
