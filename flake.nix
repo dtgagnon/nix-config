ab0a5166f2e4cbd76d2d62e34f7769b3042c001c
dtgagnon <gagnon.derek@gmail.com>
2024-11-18 14:49:24 -0500

    switching to remote secrets repo

--- flake.nix
+++ flake.nix
@@ -29,8 +29,8 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "stablepkgs";
     nix-secrets = {
-      url = "/home/dtgagnon/nix-config/nix-secrets";
-#      url = "git+ssh://git@github.com/dtgagnon/nix-secrets.git";
+      # url = "/home/dtgagnon/nix-config/nix-secrets";
+      url = "git+ssh://git@github.com/dtgagnon/nix-secrets.git";
       flake = false;
     };
 
