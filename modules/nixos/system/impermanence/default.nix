6c710a1a2be392e4740d3a69b424b6f6f283fd26
dtgagnon <gagnon.derek@gmail.com>
2024-11-18 13:30:26 -0500

    insignif

--- modules/nixos/system/impermanence/default.nix
+++ modules/nixos/system/impermanence/default.nix
@@ -20,7 +20,7 @@ in
 
   config = mkIf cfg.enable {
     fileSystems."/persist".neededForBoot = true;
-    environment.persistence."/persist/system" = {
+    environment.persistence."/persist" = {
       hideMounts = true;
       directories = [
         "/etc/nixos"
