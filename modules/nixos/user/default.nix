09c61772788c355820c73b0dd3aff98d0588ca5b
dtgagnon <gagnon.derek@gmail.com>
2024-11-18 09:58:17 -0500

    giving up on abstracting system user creation based on homes directory with snowfall-lib

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -6,13 +6,12 @@
 }:
 let
   inherit (lib) types;
-  inherit (lib.${namespace}) mkOpt mkBoolOpt;
+  inherit (lib.${namespace}) mkOpt mkBoolOpt homesUserName;
   cfg = config.${namespace}.user;
-  username = config.snowfallorg.user.name;
 in
 {
   options.${namespace}.user = with types; {
-    name = mkOpt str "${username}" "The name to use for the user account";
+    name = mkOpt str "dtgagnon" "The name to use for the user account"
     fullName = mkOpt str "" "The full name of the user";
     email = mkOpt str "" "The email of the user";
     # initialPassword = mkOpt str "password" "The initial password for the user account.";
@@ -21,24 +20,25 @@ in
     shell = mkOpt pkgs nushell "The user's default shell";
     prompt-init = mkBoolOpt true "Whether or not to show an initial message when opening a new shell";
 
-    mkAdmin = mkBoolOpt (if "${username}" == "dtgagnon" || "admin" || "root" then true else false) "Declare if the user should be added to wheel automatically";
+    mkAdmin = mkBoolOpt (if "${cfg.name}" == "dtgagnon" || "admin" || "root" then true else false) "Declare if the user should be added to wheel automatically";
   };
 
   config = {
     users.users.${cfg.name} = {
-      hashedPasswordFile = config.sops.secrets."${username}-password.path";
-      home = "/persist/home/${username}";
+      hashedPasswordFile = config.sops.secrets."${cfg.name}-password.path";
+      home = "/persist/home/${cfg.name}";
       group = "users";
       extraGroups = cfg.extraGroups;
       isNormalUser = true; # If false, the user is treated as a 'system user'.
       inherit (cfg) shell;
     } // cfg.extraOptions;
 
-    snowfallorg.users.${username}.admin = cfg.mkAdmin;
+    snowfallorg.users.${cfg.name}.admin = cfg.mkAdmin;
 
     # User security
     ## Decrypts user's password from secrets.yaml to /run/secrets-for-users/ so it can be used to create users
-    sops.secrets."${username}-password".neededForUsers = true;
+    sops.secrets."${cfg.name}-password".neededForUsers = true;
     users.mutableUsers = false; # Required for password to be set via sops during system activation. Forces user settings to be declared via config exclusively
   };
+
 }
