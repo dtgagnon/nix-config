2759f744fa61974fe9637befc5b1aa312bf85dc4
dtgagnon <gagnon.derek@gmail.com>
2024-11-19 12:59:59 -0500

    tweaking openssh config

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -8,11 +8,13 @@
 }:
 let
   inherit (lib) mkIf optionalString types foldl;
-  inherit (lib.${namespace}) mkOpt mkBoolOpt hasOptinPersistence;
+  inherit (lib.${namespace}) mkOpt mkBoolOpt;
   cfg = config.${namespace}.services.openssh;
 
   user = config.${namespace}.user.name;
   user-id = builtins.toString user.uid;
+
+  hasOptinPersistence = config.environment.persistence ? "/persist";
 
   # TODO: This is a hold-over from old snowfall-lib rev using specialArg `name` to provide hostname. Can be moved to just `host` once the `name` uses are identified.
   name = host;
@@ -78,7 +80,7 @@ in
 
       hostKeys = [
         {
-          path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
+          path = "${optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
           type = "ed25519";
         }
       ];
@@ -107,5 +109,9 @@ in
 
     networking.firewall.allowedTCPPorts = [ cfg.port ];
 
+    security.pam.sshAgentAuth = {
+      enabled = true;
+      authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/%u" ];
+    };
   };
 }
