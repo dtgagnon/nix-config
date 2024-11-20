4b8bc7a6c75d5727649317e96dee72a1462d0d46
dtgagnon <gagnon.derek@gmail.com>
2024-11-19 19:24:40 -0500

    added wallpapers

--- modules/home/desktop/stylix/default.nix
+++ modules/home/desktop/stylix/default.nix
@@ -23,7 +23,7 @@ in
       enable = true;
       polarity = "dark"; # "light" || "dark" || "either"
 
-      image = mkIf (cfg.themeSource == ".config/wallpaper/nix-wallpaper-gear.png") "need to put a wallpaper directory here";
+      image = mkIf (cfg.themeSource == "image") "~/.config/wallpaper/nix-wallpaper-gear.png";
       base16Scheme = mkIf (cfg.themeSource == "base16Scheme") "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
       # base16Scheme = {
       #   base00 = "";
