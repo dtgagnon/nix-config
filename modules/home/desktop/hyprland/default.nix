a280400e4bb0529596ea1003d01bbc0f32ea7bfb
dtgagnon <gagnon.derek@gmail.com>
2024-11-20 22:31:48 -0500

    tweaking hyprland config

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -59,7 +59,7 @@ in
     systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
     wayland.windowManager.hyprland = {
       enable = true;
-      xwayland = { enable = true; hidpi = true; };
+      xwayland.enable = true;
       # enableNvidiaPatches = false;
       systemd.enable = true;
 
