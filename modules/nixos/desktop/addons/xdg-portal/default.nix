b082efc6e42f6e3714bd46ceb972db3b73ebda28
dtgagnon <gagnon.derek@gmail.com>
2024-11-26 15:15:52 -0500

    minor correction

--- modules/nixos/desktop/addons/xdg-portal/default.nix
+++ modules/nixos/desktop/addons/xdg-portal/default.nix
@@ -20,7 +20,6 @@ in
       portal = {
         enable = true;
         extraPortals = with pkgs; [
-          xdg-desktop-portal-hyprland
           xdg-desktop-portal-wlr
           xdg-desktop-portal-gtk
         ];
