9ced065ce4683f226dcdfc29b038078d332f40cd
dtgagnon <gagnon.derek@gmail.com>
2024-12-01 23:19:41 -0500

    trying to fix windsurf not retaining user auth tokens from keyring

--- modules/home/apps/windsurf/default.nix
+++ modules/home/apps/windsurf/default.nix
@@ -17,6 +17,23 @@ in
   config = mkIf cfg.enable {
     home.packages = [ pkgs.spirenix.windsurf ];
 
+		home.file."~/.windsurf/argv.json".text = ''
+      // This configuration file allows you to pass permanent command line arguments to VS Code.
+      {
+        // For compatibility with Hyprland, to get it to know what keyring to use.
+        "password-store":"gnome-libsecret"
+
+        // Use software rendering instead of hardware accelerated rendering. This can help in cases where you see rendering issues in VS Code.
+        // "disable-hardware-acceleration": true,
+
+        // Allows to disable crash reporting. Should restart the app if the value is changed.
+        "enable-crash-reporter": true,
+
+        // Unique id used for correlating crash reports sent from this instance. Do not edit this value.
+        "crash-reporter-id": "4c9b8afe-4e3d-40db-a77d-3879fc1923bd"
+      }
+		'';
+
     spirenix.user.persistHomeDirs = [
       ".config/Windsurf"  # Future XDG config location
       ".windsurf"         # Current data directory
