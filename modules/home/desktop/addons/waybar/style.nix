26c37d4674629be4c9d7a641bae840fb776df1d4
dtgagnon <gagnon.derek@gmail.com>
2024-12-03 12:36:47 -0500

    tweaking waybar

--- modules/home/desktop/addons/waybar/style.nix
+++ modules/home/desktop/addons/waybar/style.nix
@@ -12,7 +12,7 @@ let
 in
 {
   config = mkIf cfg.enable {
-    programs.waybar.style = concatStrings [ ${cfg.extraStyle} + ''
+    programs.waybar.style = concatStrings [ "${cfg.extraStyle}" + ''
       * {
         font-family: JetBrainsMono Nerd Font Mono;
         font-size: 16px;
