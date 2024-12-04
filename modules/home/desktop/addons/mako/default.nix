3756bee0e4392b91b3ea254004a7e043b01ff71d
dtgagnon <gagnon.derek@gmail.com>
2024-12-04 17:38:30 -0500

    added mako and adjusted styles for rofi and waybar

--- modules/home/desktop/addons/mako/default.nix
+++ modules/home/desktop/addons/mako/default.nix
@@ -8,9 +8,6 @@ let
   inherit (lib) mkIf;
   inherit (lib.${namespace}) mkBoolOpt;
   cfg = config.${namespace}.desktop.addons.mako;
-
-  inherit (config.lib.stylix) colors;
-  fontSize = toString config.stylix.fonts.sizes.desktop;
 in
 {
   options.${namespace}.desktop.addons.mako = {
@@ -18,15 +15,11 @@ in
   };
 
   config = mkIf cfg.enable {
-    # home.packages = [ pkgs.libnotify ];
+    home.packages = [ pkgs.libnotify ];
 
     services.mako = {
       enable = true;
-      # font = "${config.stylix.fonts.sansSerif.name} ${fontSize}";
       borderRadius = 4;
-      # textColor = "${colors.base07}ff";
-      # backgroundColor = "${colors.base00}f4";
-      # borderColor = "${colors.base03}ff";
       borderSize = 2;
       margin = "12,12,6";
       padding = "12,12,12,12";
--- modules/home/desktop/styling/stylix/default.nix
+++ modules/home/desktop/styling/stylix/default.nix
@@ -65,7 +65,7 @@ in
 
       opacity = {
         applications = 1.0;
-        terminal = 0.8;
+        terminal = 0.75;
         desktop = 1.0;
         popups = 1.0;
       };
