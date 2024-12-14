127c082c678173371d4497a6ec56d87577824013
dtgagnon <gagnon.derek@gmail.com>
2024-12-13 23:24:07 -0500

    added freeCAD

---
+++ modules/home/apps/freecad/default.nix
@@ -0,0 +1,20 @@
+{ 
+  lib,
+  pkgs,
+  config,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.apps.freecad;
+in {
+  options.${namespace}.apps.freecad = {
+    enable = mkBoolOpt false "Enable FreeCAD";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.freecad-wayland ];
+  };
+}
