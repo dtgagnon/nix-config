8d1adca5a586c953a54e359a0c07fa99d4ef5826
dtgagnon <gagnon.derek@gmail.com>
2024-12-15 22:01:05 -0500

    moved directories around

--- modules/nixos/apps/proton-cloud/default.nix
+++ modules/nixos/apps/proton-cloud/default.nix
@@ -16,7 +16,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
+    environment.systemPackages = with pkgs; [
       proton-pass
       protonvpn-gui
       protonmail-desktop
