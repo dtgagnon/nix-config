3ce0bbdf55902ed3f556567857d834b00487dd0f
dtgagnon <gagnon.derek@gmail.com>
2024-12-27 15:53:47 -0500

    setting ghostty theme

--- modules/home/apps/terminals/ghostty/default.nix
+++ modules/home/apps/terminals/ghostty/default.nix
@@ -1,10 +1,9 @@
-{
-  lib,
-  config,
-  inputs,
-  system,
-  namespace,
-  ...
+{ lib
+, config
+, inputs
+, system
+, namespace
+, ...
 }:
 let
   inherit (lib) mkIf types;
@@ -23,37 +22,38 @@ in
     home.packages = [ inputs.ghostty.packages.${system}.default ];
 
     xdg.configFile."ghostty/config".text = ''
-      			font-family =
-      			font-size = 14
-      			adjust-cell-width =
-      			adjust-cell-height =
-      			adjust-underline-thickness =
+      						theme = gruvbox-material
+            			font-family =
+            			font-size = 14
+            			adjust-cell-width =
+            			adjust-cell-height =
+            			adjust-underline-thickness =
 
-      			window-padding-x =
-      			window-padding-y =
+            			window-padding-x =
+            			window-padding-y =
 
-      			background = ${colors.base00}
-      			foreground = ${colors.base05}
+            			background = ${colors.base00}
+            			foreground = ${colors.base05}
 
-      			selection-background = ${colors.base06}
-      			selection-foreground = ${colors.base08}
+            			selection-background = ${colors.base06}
+            			selection-foreground = ${colors.base08}
 
-      			palette = 0=#${colors.base00}
-      			palette = 1=#${colors.base01}
-      			palette = 2=#${colors.base02}
-      			palette = 3=#${colors.base03}
-      			palette = 4=#${colors.base04}
-      			palette = 5=#${colors.base05}
-      			palette = 6=#${colors.base06}
-      			palette = 7=#${colors.base07}
-      			palette = 8=#${colors.base08}
-      			palette = 9=#${colors.base09}
-      			palette = 10=#${colors.base0A}
-      			palette = 11=#${colors.base0B}
-      			palette = 12=#${colors.base0C}
-      			palette = 13=#${colors.base0D}
-      			palette = 14=#${colors.base0E}
-      			palette = 15=#${colors.base0F}
-      		'';
+            			palette = 0=#${colors.base00}
+            			palette = 1=#${colors.base01}
+            			palette = 2=#${colors.base02}
+            			palette = 3=#${colors.base03}
+            			palette = 4=#${colors.base04}
+            			palette = 5=#${colors.base05}
+            			palette = 6=#${colors.base06}
+            			palette = 7=#${colors.base07}
+            			palette = 8=#${colors.base08}
+            			palette = 9=#${colors.base09}
+            			palette = 10=#${colors.base0A}
+            			palette = 11=#${colors.base0B}
+            			palette = 12=#${colors.base0C}
+            			palette = 13=#${colors.base0D}
+            			palette = 14=#${colors.base0E}
+            			palette = 15=#${colors.base0F}
+            		'';
   };
 }
