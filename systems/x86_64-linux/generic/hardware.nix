1dd5a1f0b689aa5ee6e745184b1e7fef04e1a97c
dtgagnon <gagnon.derek@gmail.com>
2025-01-06 17:50:15 -0500

    declared monitor config for hyprland

--- systems/x86_64-linux/generic/hardware.nix
+++ systems/x86_64-linux/generic/hardware.nix
@@ -1,1 +1,9 @@
-throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config`?"
+{ system
+, ...
+}:
+let
+  reminder = if system == "generic" then throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config`?" else null;
+in
+{
+  inherit reminder;
+}
