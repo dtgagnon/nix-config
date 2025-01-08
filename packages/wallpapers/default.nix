d9b06537d514ca4a606af57228aa271c357e2832
dtgagnon <gagnon.derek@gmail.com>
2025-01-08 15:10:16 -0500

    test for wallpaper subdirs

--- packages/wallpapers/default.nix
+++ packages/wallpapers/default.nix
@@ -14,55 +14,62 @@ let
         cp $src $out
       '';
       passthru = {
-        # e.g., so you can do pkgs.<namespace>.wallpapers.<something>.fileName
+        # For debugging/reference
         fileName = builtins.baseNameOf src;
       };
     };
 
-  # Recursively crawl a directory (dir) building an attribute set:
-  # - If an entry is a directory => recurse and build a sub-attribute set
-  # - If an entry is a file => build a mkWallpaper derivation
-  buildWallpapers = dir: 
-  let
-    contents = builtins.readDir dir;    # readDir returns an attrset of { <fileName> = { ... } }
-    names    = builtins.attrNames contents;
-  in
-    lib.attrsets.fromList (lib.map (entry:
-    let
-      path     = "${dir}/${entry}";
-      isDir    = contents.${entry}.directory;  # boolean
-      nameNoExt = lib.snowfall.path.get-file-name-without-extension entry;
-    in
-      if isDir then
-        # subdirectory => build a nested attribute set
-        {
-          name  = entry;
-          value = buildWallpapers path;
-        }
-      else
-        # file => build a derivation for the wallpaper
-        {
-          name  = nameNoExt;
-          value = mkWallpaper nameNoExt path;
-        }
-    ) names);
+  # If you're using lib.snowfall.path.get-file-name-without-extension,
+  # be sure that function is actually in scope. Otherwise define your own,
+  # for example:
+  #
+  #  getFileNameWithoutExtension = file: builtins.head (builtins.splitVersion file);
+  #
+  # For now, we'll assume it's available via lib.snowfall.
+
+  # Recursively crawl a directory building an attribute set:
+  #   - If an entry is a directory, recurse to build a sub-attribute set
+  #   - If an entry is a file, build a mkWallpaper derivation
+  buildWallpapers =
+    dir:
+      let
+        contents = builtins.readDir dir;
+        names    = builtins.attrNames contents;
+      in
+        builtins.listToAttrs (map (entry: let
+          path     = "${dir}/${entry}";
+          isDir    = contents.${entry}.directory; 
+          nameNoExt = lib.snowfall.path.get-file-name-without-extension entry;
+        in
+          if isDir then
+            {
+              name  = entry;                   # sub-attribute name is the folder name
+              value = buildWallpapers path;    # recurse
+            }
+          else
+            {
+              name  = nameNoExt;              # attribute name is the file name w/o extension
+              value = mkWallpaper nameNoExt path;
+            }
+        ) names);
 
   # Build the nested attribute set starting from ./wallpapers
   wallpapers = buildWallpapers ./wallpapers;
 
 in
-# Now create the "umbrella" derivation that installs them
+# Main derivation that installs everything into $out/share/wallpapers
 pkgs.stdenvNoCC.mkDerivation {
   name = "wallpapers";
   src  = ./wallpapers;
 
   installPhase = ''
-    mkdir -p $out/share/wallpapers
-    # Copy everything under ./wallpapers into $out/share/wallpapers
-    cp -r ./* $out/share/wallpapers
+    mkdir -p "$out/share/wallpapers"
+    cp -r ./* "$out/share/wallpapers"
   '';
 
-  # Expose your entire nested set of wallpaper derivations in passthru
-  # so that referencing pkgs.<namespace>.wallpapers.<subdir>.<imageName> works.
-  passthru = wallpapers;
+  # Expose your entire nested set of wallpaper derivations
+  passthru = {
+    # Optionally, you can also expose any top-level "names" you want here
+    inherit wallpapers;
+  };
 }
