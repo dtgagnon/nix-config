e44ae1f447d15c39d9e78416b4b6101380d40358
dtgagnon <gagnon.derek@gmail.com>
2025-01-08 15:06:38 -0500

    test for wallpaper subdirs

--- packages/wallpapers/default.nix
+++ packages/wallpapers/default.nix
@@ -1,6 +1,3 @@
-# This is a Nix derivation for managing system wallpapers
-# It takes standard Nix inputs: lib (for helper functions), pkgs (for dependencies),
-# config (for system configuration), and namespace
 { lib
 , pkgs
 , config
@@ -8,79 +5,64 @@
 , ...
 }:
 let
-  # Get a list of all wallpaper filenames from the ./wallpapers directory
-  images = builtins.attrNames (builtins.readDir ./wallpapers);
-
-  # Helper function to create a Nix derivation for a single wallpaper
-  # Takes a name and source path as arguments
+  # Reusable function to build a wallpaper derivation
   mkWallpaper = name: src:
-    let
-      # Extract just the filename from the full path
-      fileName = builtins.baseNameOf src;
-      # Create a simple derivation that just copies the wallpaper file
-      pkg = pkgs.stdenvNoCC.mkDerivation {
-        inherit name src;
-
-        # No need to unpack since we're just copying files
-        dontUnpack = true;
-
-        # Simple installation: just copy the source file to the output
-        installPhase = ''
-          cp $src $out
-        '';
-
-        # Make the filename available to other parts of the configuration
-        passthru = {
-          inherit fileName;
-        };
+    pkgs.stdenvNoCC.mkDerivation {
+      inherit name src;
+      dontUnpack = true;
+      installPhase = ''
+        cp $src $out
+      '';
+      passthru = {
+        # e.g., so you can do pkgs.<namespace>.wallpapers.<something>.fileName
+        fileName = builtins.baseNameOf src;
       };
+    };
+
+  # Recursively crawl a directory (dir) building an attribute set:
+  # - If an entry is a directory => recurse and build a sub-attribute set
+  # - If an entry is a file => build a mkWallpaper derivation
+  rec buildWallpapers = dir: 
+  let
+    contents = builtins.readDir dir;    # readDir returns an attrset of { <fileName> = { ... } }
+    names    = builtins.attrNames contents;
+  in
+    lib.attrsets.fromList (lib.map (entry:
+    let
+      path     = "${dir}/${entry}";
+      isDir    = contents.${entry}.directory;  # boolean
+      nameNoExt = lib.snowfall.path.get-file-name-without-extension entry;
     in
-    pkg;
+      if isDir then
+        # subdirectory => build a nested attribute set
+        {
+          name  = entry;
+          value = buildWallpapers path;
+        }
+      else
+        # file => build a derivation for the wallpaper
+        {
+          name  = nameNoExt;
+          value = mkWallpaper nameNoExt path;
+        }
+    ) names);
 
-  # Create a list of wallpaper names without their file extensions
-  names = builtins.map (lib.snowfall.path.get-file-name-without-extension) images;
+  # Build the nested attribute set starting from ./wallpapers
+  wallpapers = buildWallpapers ./wallpapers;
 
-  # Create an attribute set mapping wallpaper names to their derivations
-  # This transforms the list of image files into a set of Nix packages
-  wallpapers = lib.foldl
-    (
-      acc: image:
-        let
-          # Get the name of the wallpaper without its extension
-          name = lib.snowfall.path.get-file-name-without-extension image;
-        in
-        # Add this wallpaper to the accumulated set
-        acc // { "${name}" = mkWallpaper name (./wallpapers + "/${image}"); }
-    )
-    { }
-    images;
-
-  # Define where wallpapers will be installed in the final system
-  installTarget = "$out/share/wallpapers";
-
-  # Create installation instructions for each wallpaper
-  installWallpapers = builtins.mapAttrs
-    (name: wallpaper: ''
-      cp ${wallpaper} ${installTarget}/${wallpaper.fileName}
-    '')
-    wallpapers;
 in
-# Main derivation that creates the complete wallpaper package
+# Now create the "umbrella" derivation that installs them
 pkgs.stdenvNoCC.mkDerivation {
   name = "wallpapers";
-  src = ./wallpapers;
+  src  = ./wallpapers;
 
-  # Installation phase: create directory and copy all wallpapers
   installPhase = ''
-    mkdir -p ${installTarget}
-
-    # Copy all files from the source directory to the installation directory
-    find * -type f -mindepth 0 -maxdepth 0 -exec cp ./{} ${installTarget}/{} ';'
+    mkdir -p $out/share/wallpapers
+    # Copy everything under ./wallpapers into $out/share/wallpapers
+    cp -r ./* $out/share/wallpapers
   '';
 
-  # Make wallpaper names and individual wallpaper derivations available
-  # to other parts of the Nix configuration
-  passthru = {
-    inherit names;
-  } // wallpapers;
+  # Expose your entire nested set of wallpaper derivations in passthru
+  # so that referencing pkgs.<namespace>.wallpapers.<subdir>.<imageName> works.
+  passthru = wallpapers;
 }
