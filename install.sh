c93876e8d39231240c4ea6eb71ad0da58862a21d
dtgagnon <gagnon.derek@gmail.com>
2025-01-15 20:10:23 -0500

    commented out keyFile to see if nixos-anywhere will proceed

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ read -p "Enter target IP address: " target_ip
 
 # Deploy using nixos-anywhere
 nix run nixpkgs#nixos-anywhere -- \
-	--disk-encrption-keys /tmp/root-crypt.key /tmp/root-crypt.key \
-	--disk-encrption-keys /tmp/data-crypt.key /tmp/data-crypt.key \
-	--flake '.#generic' \
+	--disk-encryption-keys /tmp/root-crypt.key /tmp/root-crypt.key \
+	--disk-encryption-keys /tmp/data-crypt.key /tmp/data-crypt.key \
 	--generate-hardware-config nixos-generate-config ./systems/x86_64-linux/generic/hardware.nix \
-	root@${target_ip}
+	--flake '.#generic' \
+	--target-host root@${target_ip}
--- systems/x86_64-linux/generic/disk-config.nix
+++ systems/x86_64-linux/generic/disk-config.nix
@@ -25,7 +25,7 @@
                 # extraOpenArgs = [ ];
                 settings = {
                   #		if you want to use the key for interactive login be sure there is no trailing newline; for example use `echo -n "password" > /tmp/secret.key`
-                  keyFile = "/tmp/root-crypt.key";
+                  # keyFile = "/tmp/root-crypt.key";
                   allowDiscards = true;
                 };
                 # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
@@ -52,7 +52,7 @@
                 # extraOpenArgs = [ ];
                 settings = {
                   #		if you want to use the key for interactive login be sure there is no trailing newline; for example use `echo -n "password" > /tmp/secret.key`
-                  keyFile = "/tmp/data-crypt.key";
+                  # keyFile = "/tmp/data-crypt.key";
                   allowDiscards = true;
                 };
                 # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
