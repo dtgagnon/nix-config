ab412468ad58812581818e61574ad9c16e71e887
dtgagnon <gagnon.derek@gmail.com>
2025-01-17 23:24:39 -0500

    added user

---
+++ homes/x86_64-linux/gac@spirepoint/default.nix
@@ -0,0 +1,75 @@
+{ lib
+, pkgs
+, config
+, osConfig ? { }
+, format ? "unknown"
+, namespace
+, ...
+}:
+let
+  inherit (lib.${namespace}) enabled mkRGBA;
+  inherit (config.lib.stylix) colors;
+in
+{
+  spirenix = {
+    user = {
+      fullName = "Gina";
+      email = "";
+    };
+
+    apps = {
+      office.okular-pdf = enabled;
+      terminals = {
+        ghostty = {
+          enable = true;
+          dark-theme = "catppuccin-frappe";
+          light-theme = "catppuccin-latte";
+        };
+      };
+      zen = enabled;
+    };
+
+    cli = {
+      eza = enabled;
+      fastfetch = enabled;
+      fzf = enabled;
+      git = enabled;
+      neovim = enabled;
+      network-tools = enabled;
+      shells.nushell = enabled;
+      ssh = enabled;
+      yazi = enabled;
+      zoxide = enabled;
+    };
+
+    desktop = {
+      hyprland = {
+        enable = true;
+        monitors = [ ", preferred, auto, 1" ];
+        extraConfig = ''
+          general {
+            col.active_border = ${mkRGBA { hex = "#${colors.base0D}"; alpha = 0.75; }}
+            col.inactive_border = ${mkRGBA { hex = "#${colors.base03}"; alpha = 0.6; }}
+          }
+        '';
+      };
+      styling.core = {
+        enable = true;
+        cursor = {
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Modern-Ice";
+          size = 24;
+        };
+        theme = "catppuccin-frappe";
+        wallpaper = pkgs.spirenix.wallpapers.wallpapers.catppuccin.flying-comets-clouds;
+      };
+      styling.stylix = {
+        enable = true;
+        polarity = "either";
+      };
+      styling.wallpapers = enabled;
+    };
+    services.syncthing = enabled;
+  };
+  home.stateVersion = "24.11";
+}
