c2b207b46c2739dcb1df48088de4a757c6dbe927
dtgagnon <gagnon.derek@gmail.com>
2025-01-21 22:16:23 -0500

    setup minimal/generic flake and config

--- systems/x86_64-linux/generic/default.nix
+++ systems/x86_64-linux/generic/default.nix
@@ -17,17 +17,26 @@
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
-        configurationLimit = 10;
+        configurationLimit = lib.mkDefault 3;
+        consoleMode = lib.mkDefault "max";
         editor = false;
       };
     };
     initrd = {
       systemd.enable = true;
+      systemd.emergencyAccess = true; # don't need to enter password in emergency mode
+      luks.forceLuksSupportInInitrd = true;
       availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" "rtsx_usb_sdmmc" ];
       kernelModules = [ "dm-snapshot" ];
     };
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
+    kernelParams = [
+      "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1"
+      "systemd.show_status=true"
+      "systemd.log_target=console"
+      "systemd.journald.forward_to_console=1"
+    ];
   };
 
   networking.networkmanager.enable = true;
@@ -59,6 +68,7 @@
   nix = {
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
     settings = {
       experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
       warn-dirty = false;
