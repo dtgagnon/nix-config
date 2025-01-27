b34fb9f0d99b0d6069decb38b98ce6f5ffd610cd
dtgagnon <gagnon.derek@gmail.com>
2025-01-27 13:21:13 -0500

    spirepoint tweaks

--- systems/x86_64-linux/spirepoint/default.nix
+++ systems/x86_64-linux/spirepoint/default.nix
@@ -20,6 +20,54 @@ in
     package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  fileSystems."/boot".options = [ "umask=0077" ];
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = lib.mkDefault 3;
+        consoleMode = lib.mkDefault "max";
+        editor = false;
+      };
+    };
+    initrd = {
+      systemd.enable = true;
+      systemd.emergencyAccess = true; # don't need to enter password in emergency mode
+      luks.forceLuksSupportInInitrd = true;
+      availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" "rtsx_usb_sdmmc" ];
+      kernelModules = [ "dm-snapshot" ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    kernelParams = [
+      "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1"
+      "systemd.show_status=true"
+      "systemd.log_target=console"
+      "systemd.journald.forward_to_console=1"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = lib.mkForce "yes";
+    authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
+  };
+
+  security.pam = {
+    rssh.enable = true;
+    services.sudo = {
+      rssh = true;
+    };
+  };
+
+  users.users.root = {
+    password = "1";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9zKXOt7YQW0NK0+GsUQh4cgmcLyurpeTzYXMYysUH1 user=dtgagnon"
+    ]; # Replace "KEY" with your public key
+  };
+
   spirenix = {
     suites = {
       gaming = enabled;
