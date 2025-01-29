29745f5e5349cbb93fa7079046d740c5319b3aa8
dtgagnon <gagnon.derek@gmail.com>
2025-01-29 13:30:02 -0500

    enabled watchers

--- modules/home/services/activity-watch/default.nix
+++ modules/home/services/activity-watch/default.nix
@@ -27,52 +27,46 @@ in
       #     aw-keywatcher = "${pkgs.aw-keywatcher}/share/aw-keywatcher/static";
       #   };
       # };
-      watchers = mkMerge [
-        {
-          aw-watcher-afk = {
-            package = pkgs.activitywatch;
-            settings = {
-              timeout = 300;
-              poll_time = 2;
-            };
+      watchers = {
+        aw-watcher-afk = {
+          package = pkgs.activitywatch;
+          settings = {
+            timeout = 300;
+            poll_time = 2;
           };
+        };
 
-          aw-watcher-window = {
-            package = pkgs.activitywatch;
-            settings = {
-              poll_time = 1;
-              # exclude_title = true;
-            };
+        aw-watcher-window = {
+          package = pkgs.activitywatch;
+          settings = {
+            poll_time = 1;
+            # exclude_title = true;
           };
+        };
 
-          aw-watcher-web = {
-            package = pkgs.activitywatch;
-          };
+        aw-watcher-web = {
+          package = pkgs.activitywatch;
+        };
 
-          aw-watcher-input = { };
+        aw-watcher-input = { };
 
-          # name-of-watcher = {
-          #   executable = "";
-          #   extraOptions = [ "" ];
-          #   name = "";
-          #   package = pkgs.activitywatch;
-          #   settings = {
-          #     # in TOML
-          #     enable_greetings = true;
-          #     poll_time = 2;
-          #     timeout = 300;
-          #   };
-          #   settingsFilename = "config.toml";
-          # };
-        }
-        mkIf
-        config.wayland.windowManager.hyprland.enable
-        {
-          aw-watcher-window-wayland = {
-            settings.poll_time = 1;
-          };
-        }
-      ];
+        # name-of-watcher = {
+        #   executable = "";
+        #   extraOptions = [ "" ];
+        #   name = "";
+        #   package = pkgs.activitywatch;
+        #   settings = {
+        #     # in TOML
+        #     enable_greetings = true;
+        #     poll_time = 2;
+        #     timeout = 300;
+        #   };
+        #   settingsFilename = "config.toml";
+        # };
+        aw-watcher-window-wayland = mkIf config.wayland.windowManager.hyprland.enable {
+          settings.poll_time = 1;
+        };
+      };
     };
   };
 }
