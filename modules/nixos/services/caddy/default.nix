2cebbb5387d3b47659cb4f27bc80504bbeda82f1
dtgagnon <gagnon.derek@gmail.com>
2025-02-05 10:50:20 -0500

    tweaking caddy, but not ready for use

--- modules/nixos/services/caddy/default.nix
+++ modules/nixos/services/caddy/default.nix
@@ -12,7 +12,6 @@ in
 {
   options.${namespace}.services.caddy = {
     enable = mkBoolOpt false "Enable caddy for reverse-proxy";
-    dataDir = mkOpt types.path "/var/lib/caddy" "The caddy data directory";
     email = mkOpt (types.nullOr types.str) null "Email address used with ACME acct w/ the CA; highly recommended";
   };
 
@@ -20,24 +19,21 @@ in
     services.caddy = {
       enable = true;
       package = pkgs.caddy;
-
       acmeCA = null; # "https://acme-v02.api.letsencrypt.org/directory"
-
-      # dataDir: If left as the default value this directory will automatically be created before the Caddy server starts, otherwise you are responsible for ensuring the directory exists with appropriate ownership and permissions. Caddy v2 replaced CADDYPATH with XDG directories. See https://caddyserver.com/docs/conventions#file-locations.
-      inherit (cfg) dataDir email;
+      inherit (cfg) email;
 
       environmentFile = "/run/secrets/caddy";
       extraConfig = "";
-      globalConfig = ''
-                				{
-                					grace_period = 5s
-        									acme_ca https://acme.zerossl.com/v2/DV90
-        									acme_eab {
-        										key_id {$EAB_KEY_ID}
-        										mac_key {$EAB_MAC_KEY}
-        									}
-                				}
-                        			'';
+      # globalConfig = ''
+      #           				{
+      #           					grace_period = 5s
+      #   									acme_ca https://acme.zerossl.com/v2/DV90
+      #   									acme_eab {
+      #   										key_id {$EAB_KEY_ID}
+      #   										mac_key {$EAB_MAC_KEY}
+      #   									}
+      #           				}
+      #                   			'';
 
       virtualHosts = {
         #    "hydra.example.com" = {
