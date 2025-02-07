79712037abc41ba0e98bca2da93cb484f1e42f21
dtgagnon <gagnon.derek@gmail.com>
2025-02-07 09:55:51 -0500

    added n8n service, not configured yet

---
+++ modules/nixos/services/n8n/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, pkgs
+, config
+, namespace
+, ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.services.n8n;
+in
+{
+  options.${namespace}.services.n8n = {
+    enable = mkBoolOpt false "Enable the n8n service";
+  };
+
+  config = mkIf cfg.enable {
+    services.n8n = {
+      enable = true;
+      openFirewall = false;
+      # sets the environment variable WEBHOOK_URL for n8n, in case we're running behind a reverse proxy. This cannot be set through configuration and must reside in an environment variable.
+      webhookUrl = "";
+      # JSON values for configuration (see n8n docs)
+      settings = { };
+    };
+  };
+}
