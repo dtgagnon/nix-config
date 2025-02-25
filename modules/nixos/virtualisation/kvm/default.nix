875d629dd427ee11fd2dcf66b43fb0a19a8742f2
dtgagnon <gagnon.derek@gmail.com>
2025-02-25 15:40:27 -0500

    breaking apart the large kvm virtualisation module

--- modules/nixos/virtualisation/kvm/default.nix
+++ modules/nixos/virtualisation/kvm/default.nix
@@ -5,15 +5,16 @@
 , ...
 }:
 let
-  inherit (lib) mkIf concatStringsSep length optionalString types;
+  inherit (lib) mkIf types;
   inherit (lib.${namespace}) mkOpt mkBoolOpt enabled;
   cfg = config.${namespace}.virtualisation.kvm;
   user = config.${namespace}.user;
 in
 {
+  imports = lib.snowfall.fs.get-non-default-nix-files ./.;
+
   options.${namespace}.virtualisation.kvm = with types; {
     enable = mkBoolOpt false "Enable KVM virtualisation";
-    vfioIds = mkOpt (listOf str) [ ] "The hardware IDs to pass through to the VM";
     platform = mkOpt (enum [ "amd" "intel" ]) "intel" "The CPU platform of the host machine";
     # Use `machinectl` and then `machinectl status <name>` to get the unit "*.scope" of the VM
     machineUnits = mkOpt (listOf str) [ ] "The systemd *.scope units to wait for before starting Scream";
@@ -24,42 +25,14 @@ in
       kernelModules = [
         "kvm-${cfg.platform}"
         "i915"
-        "vfio"
-        "vfio_pci"
-        "vfio_virqfd"
-        "vfio_iommu_type1"
-        "kvmfr"
         "vhost"
         "vhost_net"
         "vhost_vsock"
         "vhost_scsi"
       ];
-      kernelParams = [
-        "${cfg.platform}_iommu=on"
-        "iommu=pt"
-        "kvm.ignore_msrs=1"
-        "vfio-pci.ids=${concatStringsSep "," cfg.vfioIds}"
-      ];
-      extraModprobeConfig = optionalString (length cfg.vfioIds > 0) ''
-        softdep nvidia pre: vfio vfio-pci
-        softdep nvidia* pre: vfio vfio-pci
-        options vfio-pci ids=${concatStringsSep "," cfg.vfioIds}
-        options kvm ignore_msrs=1
-        options kvm report_ignored_msrs=0
-        options kvmfr static_size_mb=64
-      '';
-      extraModulePackages = [ config.boot.kernelPackages.kvmfr ];
     };
 
     services.qemuGuest.enable = true;
-
-    services.udev.extraRules = ''
-      SUBSYSTEM=="kvmfr", OWNER="${user.name}", GROUP="qemu-libvirtd", MODE="0660"
-    '';
-
-    systemd.tmpfiles.rules = [
-      "f /dev/shm/looking-glass 0600 ${user.name} qemu-libvirtd -"
-    ];
 
     environment.systemPackages = with pkgs; [
       virt-manager
@@ -86,15 +59,13 @@ in
       spiceUSBRedirection.enable = true;
       libvirtd = {
         enable = true;
+        onBoot = "ignore";
+        onShutdown = "shutdown";
+        allowedBridges = [ "virbr0" "br0" ];
         extraConfig = ''
           user = "${user.name}"
           group = "qemu-libvirtd"
         '';
-
-        allowedBridges = [ "virbr0" "br0" ];
-
-        onBoot = "ignore";
-        onShutdown = "shutdown";
 
         qemu = {
           package = pkgs.qemu_kvm;
@@ -104,6 +75,7 @@ in
             packages = [ pkgs.OVMFFull.fd ];
           };
           swtpm = enabled;
+          vhostUserPackages = [ pkgs.virtiofsd ];
           verbatimConfig = ''
             namespaces = []
             user = "+${builtins.toString config.users.users.${user.name}.uid}"
