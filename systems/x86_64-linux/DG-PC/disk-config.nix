07d85c25d11bfab8b40153a1c3e2ccfc3ba248b6
dtgagnon <gagnon.derek@gmail.com>
2025-03-02 09:50:31 -0500

    updated for addition of new phyiscal volume

--- systems/x86_64-linux/DG-PC/disk-config.nix
+++ systems/x86_64-linux/DG-PC/disk-config.nix
@@ -1,42 +1,63 @@
 {
   disko.devices = {
     disk = {
-      main = {
+      primary = {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_1000GB_22292W803385";
         content = {
           type = "gpt";
           partitions = {
-            esp = {
-              name = "ESP";
+            ESP = {
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
-              name = "root";
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                    mountOptions = [ ];
-                  };
-                  "/nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [ "subvol=nix" "noatime" ];
-                  };
-                  "/persist" = {
-                    mountpoint = "/persist";
-                    mountOptions = [ "subvol=persist" "noatime" ];
-                  };
-                };
+                type = "lvm_pv";
+                vg = "root-pool";
+              };
+            };
+          };
+        };
+      };
+      secondary = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_23280Q800651";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "root-pool";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      root-pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-L" "nixos" "-f" ];
+              subvolumes = {
+                "/root" = { mountpoint = "/"; };
+                "/home" = { mountpoint = "/home"; mountOptions = [ "subvol=home" "compress=zstd" "noatime" ]; };
+                "/persist" = { mountpoint = "/persist"; mountOptions = [ "subvol=persist" "compress=zstd" "noatime" ]; };
+                "/nix" = { mountpoint = "/nix"; mountOptions = [ "subvol=nix" "compress=zstd" "noatime" ]; };
               };
             };
           };
