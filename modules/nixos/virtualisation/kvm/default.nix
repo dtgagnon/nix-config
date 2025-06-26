71cc693bc4ff509c50e944225cf5a64a8c6a13f4
dtgagnon <gagnon.derek@gmail.com>
2025-06-26 16:57:27 -0400

    feat: migrate KVM configuration to NixVirt and add Windows 11 GPU passthrough VM

--- modules/nixos/virtualisation/kvm/default.nix
+++ modules/nixos/virtualisation/kvm/default.nix
@@ -49,43 +49,7 @@ in
         '';
       };
 
-      #NOTE: virtualisation.libvirt (not libvirtd, notice the `d`) is via NixVirt flake
       virtualisation = {
-        libvirt = {
-          enable = true;
-          connections."qemu:///system" = {
-            domains = [
-              { definition = ./vm-definitions/win11-GPU.xml; }
-            ];
-            pools = [
-              {
-                definition = inputs.NixVirt.lib.pool.writeXML {
-                  name = "default";
-                  uuid = "ec93320c-83fc-4b8d-a67d-2eef519cc3fd";
-                  type = "dir";
-                  target.path = "/var/lib/libvirt/images";
-                };
-              }
-              {
-                definition = inputs.NixVirt.lib.pool.writeXML {
-                  name = "isos";
-                  uuid = "7f532314-d910-4237-99ed-ca3441e006a1";
-                  type = "dir";
-                  target.path = "/var/lib/libvirt/isos";
-                };
-              }
-              {
-                definition = inputs.NixVirt.lib.pool.writeXML {
-                  name = "nvram";
-                  uuid = "adda15d7-edf3-4b16-a968-19317c30805a";
-                  type = "dir";
-                  target.path = "/var/lib/libvirt/qemu/nvram";
-                };
-              }
-            ];
-          };
-        };
-
         libvirtd = {
           enable = true;
           onBoot = "ignore";
---
+++ modules/nixos/virtualisation/kvm/nixvirt.nix
@@ -0,0 +1,56 @@
+{ lib
+, pkgs
+, config
+, inputs
+, namespace
+, ...
+}:
+let
+  inherit (lib) mkIf mkOption types;
+  inherit (inputs) NixVirt;
+  cfg = config.${namespace}.virtualisation.kvm;
+in
+{
+  options.${namespace}.virtualisation.kvm = {
+    vmDomains = mkOption {
+      type = types.listOf types.str;
+      default = [ "win11-GPU" ];
+      description = "List of VM domains to define";
+    };
+  };
+
+  config = mkIf (cfg.enable && builtins.hasAttr "NixVirt" inputs) {
+    virtualisation.libvirt = {
+      enable = true;
+      connections."qemu:///system" = {
+        domains = builtins.map (name: { definition = builtins.getAttr name (pkgs.callPackage ./vm-definitions/${name}.nix {}); }) cfg.vmDomains;
+        pools = [
+          {
+            definition = NixVirt.lib.pool.writeXML {
+              name = "default";
+              uuid = "ec93320c-83fc-4b8d-a67d-2eef519cc3fd";
+              type = "dir";
+              target.path = "/var/lib/libvirt/images";
+            };
+          }
+          {
+            definition = NixVirt.lib.pool.writeXML {
+              name = "isos";
+              uuid = "7f532314-d910-4237-99ed-ca3441e006a1";
+              type = "dir";
+              target.path = "/var/lib/libvirt/isos";
+            };
+          }
+          {
+            definition = NixVirt.lib.pool.writeXML {
+              name = "nvram";
+              uuid = "adda15d7-edf3-4b16-a968-19317c30805a";
+              type = "dir";
+              target.path = "/var/lib/libvirt/qemu/nvram";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
