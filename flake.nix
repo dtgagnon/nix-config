6b66a84b488468015d7509b43c1fb2d5196cfa41
dtgagnon <gagnon.derek@gmail.com>
2026-01-02 21:33:09 -0500

    feat(flake): update yell input to improved-smart-commands branch
    Updates yell flake input to use feat/improved-smart-commands branch
    and regenerates lock file with latest versions from upstream inputs.

--- flake.nix
+++ flake.nix
@@ -223,7 +223,7 @@
     ## custom repos
     n8n-private.url = "git+ssh://git@github.com/dtgagnon/n8n-nix-overlay";
     odooAdds.url = "git+ssh://git@github.com/dtgagnon/odooAdds";
-    yell.url = "git+ssh://git@github.com/dtgagnon/yell";
+    yell.url = "git+ssh://git@github.com/dtgagnon/yell?ref=feat/improved-smart-commands";
     yell.inputs.nixpkgs.follows = "nixpkgs";
 
     ## ai tools
