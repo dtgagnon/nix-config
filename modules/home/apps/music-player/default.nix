25bc53c1b51657d9d54625320f28c862c5e3fa6d
dtgagnon <gagnon.derek@gmail.com>
2026-01-26 15:43:41 -0500

    fix(rmpc): use Option type for status_update_interval_ms
    rmpc config expects Option<u64> for this field, not a bare integer.

--- modules/home/apps/music-player/default.nix
+++ modules/home/apps/music-player/default.nix
@@ -31,7 +31,7 @@ in
           wrap_navigation: false,
           enable_mouse: true,
           enable_config_hot_reload: true,
-          status_update_interval_ms: 1000,
+          status_update_interval_ms: Some(1000),
           select_current_song_on_change: false,
           browser_song_sort: [Disc, Track, Artist, Title],
         )
