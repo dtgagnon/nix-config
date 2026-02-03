b1ce258fe157178905aba3e093bef3ac255dd0d5
dtgagnon <gagnon.derek@gmail.com>
2026-02-02 22:12:51 -0500

    chore(flake): switch emma input from local path to GitHub
    Replace local development path with remote git URL for reproducibility
    across machines. Also updates nix-secrets input.

--- flake.nix
+++ flake.nix
@@ -256,7 +256,7 @@
     n8n-private.url = "git+ssh://git@github.com/dtgagnon/n8n-nix-overlay";
     odooAdds.url = "git+ssh://git@github.com/dtgagnon/odooAdds";
     yell.url = "git+ssh://git@github.com/dtgagnon/yell";
-    emma.url = "path:/home/dtgagnon/proj/AUTOMATE/email-agent";
+    emma.url = "git+ssh://git@github.com/dtgagnon/emma";
 
     ## websites
     dtge.url = "git+ssh://git@github.com/dtgagnon/dtg-engineering?ref=feat/nix-deploy";
