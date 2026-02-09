b1158d28ff3d331509ff8089788b85fd6a140b9d
dtgagnon <gagnon.derek@gmail.com>
2026-02-09 12:10:01 -0500

    fix(system): set TERMINAL to executable name instead of terminfo value
    TERMINAL was set to "xterm-ghostty" (terminfo identifier) rather than
    "ghostty" (the actual binary). This broke any script using $TERMINAL -e
    to launch a terminal window.

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -96,7 +96,7 @@ in
       variables = {
         EDITOR = "nvim";
         SHELL = "nu";
-        TERMINAL = "xterm-ghostty";
+        TERMINAL = "ghostty";
         LESSHISTFILE = "$HOME/.cache/less.history";
         WGETRC = "$HOME/.config/wgetrc";
       } // cfg.environment.variables;
