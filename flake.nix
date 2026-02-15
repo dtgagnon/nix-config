ba0a554902b7ffd303d4e66507c8e39e5bd8d91a
dtgagnon <gagnon.derek@gmail.com>
2026-02-15 16:30:12 -0500

    test(hyprland): override aquamarine with readBuffer fix fork
    Point hyprland's aquamarine input to dtgagnon/aquamarine on the
    fix/readbuffer-preserve-alive-attachments branch to test the fix for
    the multi-GPU SEGV in CDRMRenderer::blit (aquamarine#246).

--- flake.nix
+++ flake.nix
@@ -226,7 +226,12 @@
     zen-browser.url = "github:0xc000022070/zen-browser-flake/beta";
 
     ## desktop
+    aquamarine = {
+      url = "github:dtgagnon/aquamarine/fix/readbuffer-preserve-alive-attachments";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.inputs.aquamarine.follows = "aquamarine";
     hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
     hyprland-plugins.inputs.hyprland.follows = "hyprland";
 
