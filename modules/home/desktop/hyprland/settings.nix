6399a1d31592abf1688e139d0f810c9a77d5748f
dtgagnon <gagnon.derek@gmail.com>
2026-02-24 16:33:29 -0500

    refactor(hyprland): change workspace animation to vertical slide with custom bezier

--- modules/home/desktop/hyprland/settings.nix
+++ modules/home/desktop/hyprland/settings.nix
@@ -74,7 +74,7 @@ in
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 5, myBezier"
-          "workspaces, 1, 6, default"
+          "workspaces, 1, 6, myBezier, slidevert"
           "specialWorkspace, 1, 6, default, fade"
         ];
       };
